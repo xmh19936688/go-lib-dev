e05c42ddbf5b7639313929062e6c9a1292d7bcaa
^_^void <xmh19936688@163.com>
2022-09-23 16:51:32 +0800

    use code on local dependency

--- example/main.go
+++ example/main.go
@@ -13,15 +13,13 @@ func main() {
 	ch := make(chan string)
 	wg := &sync.WaitGroup{}
 	d := dispatcher.New[string]().Chan(ch).WaitGroup(wg).Handler(handler)
-	go d.Start()
+	go d.Run()
 
 	data := []string{"1", "2"}
 	wg.Add(len(data))
 	for _, str := range data {
 		ch <- str
 	}
-
-	wg.Wait()
 }
 
 func handler(arg string) {
