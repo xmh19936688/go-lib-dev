1f39ab0960794b97349f6efbd7d75d4b04bcd222
^_^void <xmh19936688@163.com>
2022-09-23 14:33:14 +0800

    add dependency in example

--- example/go.mod
+++ example/go.mod
@@ -1,3 +1,5 @@
 module example
 
 go 1.19
+
+require github.com/xmh19936688/go-dispatcher v1.0.0
--- example/main.go
+++ example/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+
+	"github.com/xmh19936688/go-dispatcher/dispatcher"
+)
 
 func main() {
 	fmt.Println("go lib example")
+
+	ch := make(chan string)
+	wg := &sync.WaitGroup{}
+	d := dispatcher.New[string]().Chan(ch).WaitGroup(wg).Handler(handler)
+	go d.Start()
+
+	data := []string{"1", "2"}
+	wg.Add(len(data))
+	for _, str := range data {
+		ch <- str
+	}
+
+	wg.Wait()
+}
+
+func handler(arg string) {
+	fmt.Println(arg)
 }
